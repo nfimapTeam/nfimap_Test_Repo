0522bdeac08bc170151d43fec0ff0a065ea37634
laon2019 <laon5987@nate.com>
2025-02-16 13:14:18 +0900

    feat: cache 설정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,27 +2,26 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_TYPE === 'DEVELOP'
-  ? 'http://127.0.0.1:8000/'
-  : process.env.REACT_APP_SERVICE_URL,
+    ? 'http://127.0.0.1:8000/'
+    : process.env.REACT_APP_SERVICE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// 요청 인터셉터
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     // 예: 토큰이 있다면 헤더에 추가
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+    config.headers["Pragma"] = "no-cache";
+    config.headers["Expires"] = "0";
+
+    // params를 활용한 추가적인 캐시 방지 (URL 변경 효과)
+    config.params = { ...config.params, _t: new Date().getTime() };
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 // 응답 인터셉터
 axiosInstance.interceptors.response.use(
