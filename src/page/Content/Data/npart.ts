99e5b9d8e5e43d1fab12db4dc9606d76737fcee9
라온 <laon@ip-172-30-1-58.ap-northeast-2.compute.internal>
2025-05-26 21:33:47 +0900

    [Refactor] 리팩토링

--- src/page/Content/Data/npart.ts
+++ src/page/Content/Data/npart.ts
@@ -1,100 +1,114 @@
 export const npart = [
   {
-    "videoId": "wpKDEWs4IkM",
-    "title": "N.Flying 6TH MINI ALBUM [야호(夜好)] Sunset 엔퍄트",
-    "thumbnail": "https://i.ytimg.com/vi/wpKDEWs4IkM/default.jpg",
-    "url": "https://www.youtube.com/watch?v=wpKDEWs4IkM",
-    "publishedAt": "2023-01-04T11:44:35Z"
+    "videoId": "DpHC2PRsTjQ",
+    "title": "N.Flying 8th Mini Album 'Dearest' 폭망 (I Like You) 엔퍄트",
+    "thumbnail": "https://i.ytimg.com/vi/DpHC2PRsTjQ/default.jpg",
+    "url": "https://www.youtube.com/watch?v=DpHC2PRsTjQ",
+    "publishedAt": "2025-05-26T03:34:10Z"
   },
   {
     "videoId": "TZFIuJ3_2Yc",
     "title": "N.Flying 1st Album 'Man on the Moon' Moonshot 엔퍄트",
     "thumbnail": "https://i.ytimg.com/vi/TZFIuJ3_2Yc/default.jpg",
     "url": "https://www.youtube.com/watch?v=TZFIuJ3_2Yc",
-    "publishedAt": "2023-01-04T11:44:42Z"
-  },
-  {
-    "videoId": "CQkawXfqntw",
-    "title": "N.Flying 6TH MINI ALBUM [야호(夜好)] ㅈㅅ (Pardon?) 엔퍄트",
-    "thumbnail": "https://i.ytimg.com/vi/CQkawXfqntw/default.jpg",
-    "url": "https://www.youtube.com/watch?v=CQkawXfqntw",
-    "publishedAt": "2023-01-04T11:44:51Z"
-  },
-  {
-    "videoId": "czJPkAi_lwg",
-    "title": "N.Flying 6TH MINI ALBUM [야호(夜好)] 4242 엔퍄트",
-    "thumbnail": "https://i.ytimg.com/vi/czJPkAi_lwg/default.jpg",
-    "url": "https://www.youtube.com/watch?v=czJPkAi_lwg",
-    "publishedAt": "2023-01-04T11:44:57Z"
-  },
-  {
-    "videoId": "55cU0o69bgs",
-    "title": "N.Flying 6TH MINI ALBUM [야호(夜好)] 굿밤 (GOOD BAM) 엔퍄트",
-    "thumbnail": "https://i.ytimg.com/vi/55cU0o69bgs/default.jpg",
-    "url": "https://www.youtube.com/watch?v=55cU0o69bgs",
-    "publishedAt": "2023-01-04T11:45:02Z"
-  },
-  {
-    "videoId": "hUii9VeaU8A",
-    "title": "N.Flying 7TH MINI ALBUM ‘So, 通 (소통)’ 아무거나 (I’M GONNA) 엔퍄트",
-    "thumbnail": "https://i.ytimg.com/vi/hUii9VeaU8A/default.jpg",
-    "url": "https://www.youtube.com/watch?v=hUii9VeaU8A",
-    "publishedAt": "2023-01-04T11:45:07Z"
+    "publishedAt": "2025-05-26T03:35:12Z"
   },
   {
     "videoId": "3X-M3Nz-ZsY",
     "title": "N.Flying 7TH MINI ALBUM ‘So, 通 (소통)’ 에요 (E-YO) 엔퍄트",
     "thumbnail": "https://i.ytimg.com/vi/3X-M3Nz-ZsY/default.jpg",
     "url": "https://www.youtube.com/watch?v=3X-M3Nz-ZsY",
-    "publishedAt": "2023-01-04T11:45:13Z"
+    "publishedAt": "2025-05-26T03:35:17Z"
   },
   {
-    "videoId": "DpHC2PRsTjQ",
-    "title": "N.Flying 8th Mini Album 'Dearest' 폭망 (I Like You) 엔퍄트",
-    "thumbnail": "https://i.ytimg.com/vi/DpHC2PRsTjQ/default.jpg",
-    "url": "https://www.youtube.com/watch?v=DpHC2PRsTjQ",
-    "publishedAt": "2023-01-04T11:46:20Z"
+    "videoId": "hUii9VeaU8A",
+    "title": "N.Flying 7TH MINI ALBUM ‘So, 通 (소통)’ 아무거나 (I’M GONNA) 엔퍄트",
+    "thumbnail": "https://i.ytimg.com/vi/hUii9VeaU8A/default.jpg",
+    "url": "https://www.youtube.com/watch?v=hUii9VeaU8A",
+    "publishedAt": "2025-05-26T03:35:22Z"
   },
   {
     "videoId": "zY-zL7iV4nI",
     "title": "N.Flying 7TH MINI ALBUM ‘So, 通 (소통)’ 아 진짜요.(Oh really.) 엔퍄트",
     "thumbnail": "https://i.ytimg.com/vi/zY-zL7iV4nI/default.jpg",
     "url": "https://www.youtube.com/watch?v=zY-zL7iV4nI",
-    "publishedAt": "2023-01-04T11:46:48Z"
+    "publishedAt": "2025-05-26T03:36:25Z"
   },
   {
-    "videoId": "bXIEU5k-QAc",
-    "title": "N.Flying - 봄이 부시게 (Spring Memories) 응원법",
-    "thumbnail": "https://i.ytimg.com/vi/bXIEU5k-QAc/default.jpg",
-    "url": "https://www.youtube.com/watch?v=bXIEU5k-QAc",
-    "publishedAt": "2023-01-04T11:47:16Z"
+    "videoId": "wpKDEWs4IkM",
+    "title": "N.Flying 6TH MINI ALBUM [야호(夜好)] Sunset 엔퍄트",
+    "thumbnail": "https://i.ytimg.com/vi/wpKDEWs4IkM/default.jpg",
+    "url": "https://www.youtube.com/watch?v=wpKDEWs4IkM",
+    "publishedAt": "2025-05-26T03:38:27Z"
   },
   {
-    "videoId": "KCjnvbvvnAI",
-    "title": "[엔플라잉] Preview 엔퍄트(응원법)",
-    "thumbnail": "https://i.ytimg.com/vi/KCjnvbvvnAI/default.jpg",
-    "url": "https://www.youtube.com/watch?v=KCjnvbvvnAI",
-    "publishedAt": "2023-01-04T11:47:22Z"
+    "videoId": "czJPkAi_lwg",
+    "title": "N.Flying 6TH MINI ALBUM [야호(夜好)] 4242 엔퍄트",
+    "thumbnail": "https://i.ytimg.com/vi/czJPkAi_lwg/default.jpg",
+    "url": "https://www.youtube.com/watch?v=czJPkAi_lwg",
+    "publishedAt": "2025-05-26T03:38:36Z"
   },
   {
-    "videoId": "HJTiA72N6Rg",
-    "title": "[이승협/엔플라잉] Anyway 엔퍄트 승협이 알려드림 ㅣ 엔피아 파트 많음/안무 있음 주의",
-    "thumbnail": "https://i.ytimg.com/vi/HJTiA72N6Rg/default.jpg",
-    "url": "https://www.youtube.com/watch?v=HJTiA72N6Rg",
-    "publishedAt": "2023-01-04T11:47:32Z"
+    "videoId": "CQkawXfqntw",
+    "title": "N.Flying 6TH MINI ALBUM [야호(夜好)] ㅈㅅ (Pardon?) 엔퍄트",
+    "thumbnail": "https://i.ytimg.com/vi/CQkawXfqntw/default.jpg",
+    "url": "https://www.youtube.com/watch?v=CQkawXfqntw",
+    "publishedAt": "2025-05-26T03:38:40Z"
+  },
+  {
+    "videoId": "55cU0o69bgs",
+    "title": "N.Flying 6TH MINI ALBUM [야호(夜好)] 굿밤 (GOOD BAM) 엔퍄트",
+    "thumbnail": "https://i.ytimg.com/vi/55cU0o69bgs/default.jpg",
+    "url": "https://www.youtube.com/watch?v=55cU0o69bgs",
+    "publishedAt": "2025-05-26T03:38:44Z"
+  },
+  {
+    "videoId": "oO9AaZ8Vjk4",
+    "title": "[N.Flying] 우당탕탕 엔퍄트 연구소 컨퍼런스 (ft. 너에게 응원법)",
+    "thumbnail": "https://i.ytimg.com/vi/oO9AaZ8Vjk4/default.jpg",
+    "url": "https://www.youtube.com/watch?v=oO9AaZ8Vjk4",
+    "publishedAt": "2025-05-26T03:35:06Z"
   },
   {
     "videoId": "rpMdDuduJ9o",
     "title": "[엔플라잉] 놔 엔퍄트(응원법)",
     "thumbnail": "https://i.ytimg.com/vi/rpMdDuduJ9o/default.jpg",
     "url": "https://www.youtube.com/watch?v=rpMdDuduJ9o",
-    "publishedAt": "2023-01-04T11:47:36Z"
+    "publishedAt": "2025-05-26T03:38:05Z"
   },
   {
-    "videoId": "BAXtv67A79s",
-    "title": "[엔플라잉 이승협] 원곡자가 알려주는 'ㅈㅅ' 응원법 (Feat. 엔콘2)",
-    "thumbnail": "https://i.ytimg.com/vi/BAXtv67A79s/default.jpg",
-    "url": "https://www.youtube.com/watch?v=BAXtv67A79s",
-    "publishedAt": "2023-01-30T19:48:54Z"
+    "videoId": "sQH_EpoCreU",
+    "title": "[엔플라잉] ANYWAY 엔퍄트(응원법)",
+    "thumbnail": "https://i.ytimg.com/vi/sQH_EpoCreU/default.jpg",
+    "url": "https://www.youtube.com/watch?v=sQH_EpoCreU",
+    "publishedAt": "2025-05-26T03:38:09Z"
+  },
+  {
+    "videoId": "KCjnvbvvnAI",
+    "title": "[엔플라잉] Preview 엔퍄트(응원법)",
+    "thumbnail": "https://i.ytimg.com/vi/KCjnvbvvnAI/default.jpg",
+    "url": "https://www.youtube.com/watch?v=KCjnvbvvnAI",
+    "publishedAt": "2025-05-26T03:38:15Z"
+  },
+  {
+    "videoId": "HJTiA72N6Rg",
+    "title": "[이승협/엔플라잉] Anyway 엔퍄트 승협이 알려드림 ㅣ 엔피아 파트 많음/안무 있음 주의",
+    "thumbnail": "https://i.ytimg.com/vi/HJTiA72N6Rg/default.jpg",
+    "url": "https://www.youtube.com/watch?v=HJTiA72N6Rg",
+    "publishedAt": "2025-05-26T03:38:23Z"
+  },
+  {
+    "videoId": "bXIEU5k-QAc",
+    "title": "N.Flying - 봄이 부시게 (Spring Memories) 응원법",
+    "thumbnail": "https://i.ytimg.com/vi/bXIEU5k-QAc/default.jpg",
+    "url": "https://www.youtube.com/watch?v=bXIEU5k-QAc",
+    "publishedAt": "2025-05-26T03:39:26Z"
+  },
+  {
+    "videoId": "h3kD4_t1Zgo",
+    "title": "[엔플라잉] N.Flying(엔플라잉) _ Rooftop(옥탑방) 응원법 fan chant",
+    "thumbnail": "https://i.ytimg.com/vi/h3kD4_t1Zgo/default.jpg",
+    "url": "https://www.youtube.com/watch?v=h3kD4_t1Zgo",
+    "publishedAt": "2025-05-26T03:42:36Z"
   }
 ]
